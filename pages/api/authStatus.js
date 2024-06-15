b08f7e4a0479bfa385f7deec7749f660ecc3318a
Glad <iamgladwin2@gmail.com>
2024-06-15 00:24:36 -0400

    change favicon

--- pages/api/authStatus.js
+++ pages/api/authStatus.js
@@ -1,6 +1,5 @@
 export default function handler(req, res) {
-  // Perform authentication check here, such as checking session, JWT, etc.
-  const isLoggedIn = req.session.user ? true : false; // Example: Check if user session exists
+  const isLoggedIn = req.session.user ? true : false;
 
   res.status(200).json({ isLoggedIn });
 }
