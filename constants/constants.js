3947c4a06b8cf8b546d75b53553cd1edb34d8a15
Glad <iamgladwin2@gmail.com>
2024-08-06 23:22:24 -0400

    change redirect url

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
 export const AUTHORIZE_URL = "https://accounts.spotify.com/authorize";
-export const REDIRECT_URI = "https://main--spotifyappv1.netlify.app/login/";
+export const REDIRECT_URI = "https://spotifyappv1.vercel.app/";
 //export const REDIRECT_URI = "http://localhost:3000/login/";
 export const SCOPES = ["user-top-read"];
